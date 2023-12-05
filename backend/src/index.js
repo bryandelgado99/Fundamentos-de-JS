46b549dd54033b2d195a7be3471c7decacdb2036
Paul Delgado <bryand9970@gmail.com>
2023-12-05 16:00:53 -0500

    Cambios de index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-/*Creación de Web Server en Express y Handlebars
+/*Creación de Web Server en Express y Handlebars*/
 const express = require('express');
 const {engine} = require('express-handlebars');
 const path = require('path');
@@ -7,101 +7,39 @@ const path = require('path');
 //Creamos la instancia del método express
 const app = express()
 app.listen(3000) //Puerto de ejecución
-
-/************************** Llamamos a handlebars *****************************/
-/*app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-//Llamamos la ruta de cambio para estos elementos
-const ruta = path.join(__dirname, "views")
-app.set('views', ruta)
-
-//Render de plantillas
-app.get('/hamburguesa/triple_western', (req, res) =>{
-    res.render('home', {layout: false})
-})
-
-app.get('/hamburguesa/menu', (req, res) =>{
-    res.render('menu', {layout: false})
-})*/
+app.use(express.json())
 
 /********************************* Rutas Públicas y Privadas ********************************/
-//Ruta privad
-
-
-/********************************* Route ********************************/
-/* Rutas de llamado al server 
+//Ruta pública
 app.get('/', (req, res) => {
     res.send("Bienvenidos a mi aplicativo")
 })
 
-app.get('/dashboard', (req, res) =>{
-    res.send("Esto es el dashboard")
-})
+//Ruta privada
+//Creamos middleware
+app.use((req, res, next)=>{
+    const{email, password} = req.body
 
-//Mandar información en formato JSON
-app.use(express.json())
-
-//Params
-app.get('/pedido/:tipo', (req, res) =>{
-    //console.log(req.params)
-    res.send(`Robalo bubuja dijo: ${req.params.tipo}`)
-})
-
-//Query params
-app.get('/search', (req, res) =>{
-    if (req.query.tipo == "sencilla"){
-        res.send("Olvidaste los pepinillos")
+    if(email == "bryand9970@gmail.com" && password == "12345"){
+        next()
     }else{
-        res.send("Estan bajo tu lengua")
+        res.send("Usuario no registrado")
     }
 })
 
-//Registrar información
-app.post('/register', (req, res) =>{
-    const {body, password} =  req.body
-    res.send(`El mail del usuario es ${body} y su clave es ${password}`) //Desestructuración
-})
-*/
-
-/*----------------------------------- Response --------------------------------------------/
-//Texto
-app.get('/hamburguesa-simple',(req, res) =>{
-    res.send("Hamburguesa - Simple")
+app.get('/pedido', (req, res) =>{
+    res.send("Esto es el dashboard")
 })
 
-//Multimedia
-app.get('/hamburguesa_doble', (req, res) =>{
-    res.sendFile('./doble.jpg',{
-        root: __dirname
-    })
-})
-
-//Documentos
-app.get('/hamburguesa_triple', (req, res) =>{
-    res.sendFile('./triple.docx',{
-        root: __dirname
-    })
-})
-
-//JSON
-app.get('/hamburguesa_mixta', (req, res) =>{
-    res.status(200).json({
-        "Tipo":"Mixta",
-        "Complementos":"Papas",
-        "Extras":"Doble Queso"
-    })
-})
-/*----------------------------------------------------------------------*/
-
-/*Si no existe ninguna ruta o la ruta es erronea
+/*Si no existe ninguna ruta o la ruta es erronea*/
 app.use((req, res) =>{
     res.send("404 - Not Found")
 })
-*/
 
-/*//Conexión al puerto-----------------------------------------------
+/******************************************************/
+//Conexión al puerto-----------------------------------------------
 try{
     console.log("Servidor ejectutado en Puerto 3000")
 }catch(error){
     console.log(error)
-}*/
+}
