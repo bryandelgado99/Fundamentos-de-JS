168f6e0f0bf8f55a3e9db7a2b579e2d8efdb82c8
Paul Delgado <bryand9970@gmail.com>
2023-12-11 16:11:51 -0500

    Server Script

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const { engine }  = require('express-handlebars')
+
 
 // Inicializaciones
 const app = express()
@@ -7,16 +9,25 @@ const app = express()
 // Configuraciones 
 app.set('port',process.env.port || 3000)
 app.set('views',path.join(__dirname, 'views'))
+app.set('views',path.join(__dirname, 'views'))
+
+//Handlebars Config
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname:'.hbs'
+}))
+app.set('view engine','.hbs')
 
 // Middlewares 
 app.use(express.urlencoded({extended:false}))
+app.use(require('./routers/portfolio.routes'))
 
 // Variables globales
 
 // Rutas 
-app.get('/',(req,res)=>{
-    res.send("Server on")
-})
+
 
 // Archivos estáticos
 app.use(express.static(path.join(__dirname,'public')))
