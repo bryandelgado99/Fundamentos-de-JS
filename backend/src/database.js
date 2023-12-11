93a574c3aef2dc258888472c97aee636fe486127
Paul Delgado <bryand9970@gmail.com>
2023-12-11 16:11:40 -0500

    Database Script

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 
+const db_name = "portfolio"
+
 //Cadena de conexión a DB
-const MONGODB_URI = 'mongodb://localhost:27017/portfolio'
+const MONGODB_URI = `mongodb://localhost:27017/${db_name}`
 
 //Crear método de conexión
 connection = async()=>{
     try {
         await mongoose.connect(MONGODB_URI)
-        console.log("Database is connected")
+        console.log(`Database is connected to ${db_name}`)
     } catch (error) {
         console.log(error);
     }
